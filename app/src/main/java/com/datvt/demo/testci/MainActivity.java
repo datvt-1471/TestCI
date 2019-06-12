2914134e3526f437389166d5c167580390ea1705
datvt-1471 <vu.tien.dat@sun-asterisk.com>
2019-06-12 18:52:08 +0700

    add file config roboletric

--- app/src/main/java/com/datvt/demo/testci/MainActivity.java
+++ app/src/main/java/com/datvt/demo/testci/MainActivity.java
@@ -2,12 +2,46 @@ package com.datvt.demo.testci;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText valueX, valueY;
+    private TextView result;
+    private Button btnSum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        btnSum.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                if (TextUtils.isDigitsOnly(valueX.getText()) && TextUtils.isDigitsOnly(valueY.getText()))
+                {
+                    Toast.makeText(MainActivity.this, "Tinh Tong", Toast.LENGTH_SHORT).show();
+                    sum();
+                }
+            }
+        });
+    }
+
+    private void initView() {
+        valueX = (EditText) findViewById(R.id.edt_x);
+        valueY = (EditText) findViewById(R.id.edt_y);
+        result = (TextView) findViewById(R.id.tv_sum);
+        btnSum = (Button) findViewById(R.id.btn_ok);
+    }
+
+    private void sum() {
+        int val1 = Integer.parseInt(valueX.getText().toString());
+        int val2 = Integer.parseInt(valueY.getText().toString());
+        int answer = val1 + val2;
+        result.setText(answer + "");
     }
 }
---
+++ app/src/test/java/com/datvt/demo/testci/MainActivityTest.java
@@ -0,0 +1,70 @@
+package com.datvt.demo.testci;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import org.junit.*;
+import org.junit.runner.*;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+    private MainActivity activity;
+
+    private EditText valueX, valueY;
+
+    private TextView result;
+
+    private Button addButton;
+
+    /**
+     * - Bắt đầu 1 test case sử dụng hàm setUp (@Before), ở đây mình đang thực hiện viết test case cho
+     * RobolectriActivity.
+     */
+    @Before
+    public void setUp() throws Exception {
+        // khơi tạo activity
+        activity = Robolectric.buildActivity(MainActivity.class)
+                .create()
+                .resume()
+                .get();
+        // khởi tạo các thành phần sử dụng trong activity
+        valueX = activity.findViewById(R.id.edt_x);
+        valueY = activity.findViewById(R.id.edt_y);
+        result = activity.findViewById(R.id.tv_sum);
+        addButton = activity.findViewById(R.id.btn_ok);
+    }
+
+    /**
+     * - Bắt đầu mỗi hàm test thêm ký tự (@Test)
+     */
+    @Test
+    public void checkActivityNotNull() throws Exception {
+        assertNotNull(activity);
+        assertNotNull(valueX);
+        assertNotNull(valueY);
+        assertNotNull(result);
+        assertNotNull(addButton);
+    }
+
+    // test case kiểm tra giá trị nhập vào là 1 số nguyên
+    @Test
+    public void testInputValue() throws Exception {
+        assertThat(TextUtils.isDigitsOnly(valueX.getText()), equalTo(true));
+        assertThat(TextUtils.isDigitsOnly(valueY.getText()), equalTo(true));
+    }
+
+    //test case kiểm tra kết quả là chính xác khi nhập (8 + 1  = 9)
+    @Test
+    public void testResult() throws Exception {
+        valueX.setText("8");
+        valueY.setText("1");
+        addButton.performClick();
+        assertThat(result.getText().toString(), equalTo("9"));
+    }
+}
